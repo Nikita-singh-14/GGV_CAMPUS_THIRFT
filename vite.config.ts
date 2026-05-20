38ed70468b4e8a3ad5ddc2490d13176382739e00
Nikita-singh-14 <nk2371174@gmail.com>
2026-05-20 10:56:27 +0530

    fix vite config and build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   //content: ["./index.html", "./src/**/*.{js,ts,tsx}"],
+//   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: { extend: {} },
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  //content: ["./index.html", "./src/**/*.{js,ts,tsx}"],
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
   plugins: [
+    react(),
     tailwindcss(),
   ],
 })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
